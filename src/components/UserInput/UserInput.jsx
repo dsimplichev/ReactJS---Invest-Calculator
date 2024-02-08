dbdb93e4f7388581acf914d0cbca3c63dd5715a3
dsimplichev <dsimplichev1948@gmail.com>
2024-02-08 10:12:15 +0000

    using useState to userinput

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react"}
+
 export default function UserInput() {
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10
+    })
+    
     return (
         <section id='user-input'>
-            
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input type='number' required />
+                </p>
+                <p>
+                <label>Anuual Investment</label>
+                    <input type='number' required/>
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>Expected Return</label>
+                    <input type='number' required/>
+                </p>
+                <p>
+                <label>Duration</label>
+                    <input type='number' required/>
+                </p>
+            </div>
         </section>
     )
 }
